5feef4681572ecbc88e38b95b0eca25031fd000d
unknown <you@example.com>
2024-03-07 17:03:26 +0800

    113-03-07

---
+++ Counter1/app/src/main/java/com/example/counter1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.counter1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void button_Click(View view){
+        int counter;
+        TextView value = (TextView) findViewById(R.id.textView); //先把ID找出來
+        counter = Integer.parseInt(value.getText().toString());//轉整數
+        counter++;
+        value.setText(Integer.toString(counter));//轉字串
+    }
+
+    public void button2_Click(View view) {
+        TextView value = (TextView) findViewById(R.id.textView);
+        value.setText(R.string.initial_value);
+    }
+}
